63eaefcc706ba93d24ff96c815ccf3afb09eca4f
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2022-01-24 09:44:25 -0500

    Output the graphs in 'results' directory

--- src/graphs.R
+++ src/graphs.R
@@ -6,8 +6,6 @@ library(tidyr)
 
 dat <- read_rds(here("data/database.rds"))
 
-# Exploratory analysis -------------
-
 # Differences between the hematocrit values obtained by centrifuge or runrun
 dat_plot <- dat %>% pivot_wider(names_from = method, values_from = hematocrit)
 
@@ -16,15 +14,17 @@ base_diff_plot <- ggplot(dat_plot) +
   geom_point(aes(id, centrifuge), color = "red") +
   geom_linerange(aes(id, ymax = runrun, ymin = centrifuge))
 
+dir <- "results/graphs"
 base_diff_plot +
   facet_wrap(vars(operator))
+ggsave(here(dir, "difference_between_operators.png"))
 
 base_diff_plot +
   facet_wrap(vars(device))
-  
+ggsave(here(dir, "difference_between_devices.png"))
 
 dat_plot <- dat_plot %>% mutate(diff = runrun - centrifuge)
 ggplot(dat_plot) +
   geom_col(aes(id, diff)) +
   facet_wrap(vars(as.factor(time_centrifuge)))
-
+ggsave(here(dir, "difference_between_time.png"))
